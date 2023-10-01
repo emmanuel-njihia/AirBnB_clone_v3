e9207d3d879cd871dfb047f7f293bc57414ccf7b
Alex Maina <lexizgichbuoy@gmail.com>
2023-10-01 12:31:09 -0700

    api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,7 +8,6 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 """variable that is an instance of Blueprint"""
 
 
-from .index import *
 from api.v1.views.amenities import *
 from api.v1.views.cities import *
 from api.v1.views.index import *
