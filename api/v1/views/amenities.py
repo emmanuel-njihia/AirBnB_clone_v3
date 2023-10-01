dae05e08ef86a668f4469341295ebf2fb6cd460a
Alex Maina <lexizgichbuoy@gmail.com>
2023-10-01 14:17:50 -0700

    ameniti

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -2,7 +2,7 @@
 """amenities for api"""
 
 from api.v1.views import app_views
-from flask import jsonify, request, abort
+from flask import jsonify, request, abort, make_response
 from models import storage
 from models.amenity import Amenity
 
