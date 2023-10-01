541daac028722ccac17e881aa341a91d156d4b54
Alex Maina <lexizgichbuoy@gmail.com>
2023-10-01 12:47:59 -0700

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,35 +9,15 @@ from api.v1.views import app_views
 
 
 app = Flask(__name__)
+
 """Register blueprint to Flask instance app"""
-
-app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-app.url_map.strict_slashes = False
-
 app.register_blueprint(app_views)
-CORS(app, resources={'/*': {'origins': app_host}})
 
 
 @app.teardown_appcontext
 def teardown(exception):
     """Method to handle teardown"""
     storage.close()
-
-
-@app.errorhandler(404)
-def error_404(error):
-    """method to handle 404 error"""
-    return jsonify(error='Not found'), 404
-
-
-@app.errorhandler(400)
-def error_400(error):
-    """method that handles 400 error"""
-    msg = 'Bad request'
-    if isinstance(error, Exception) and hasattr(error, 'description'):
-        msg = error.description
-    return jsonify(error=msg), 400
 
 
 if __name__ == "__main__":
