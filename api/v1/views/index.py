28da51e48d6a53657370c2a478ec9720600c6dae
Alex Maina <lexizgichbuoy@gmail.com>
2023-10-01 12:57:48 -0700

    some_stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,12 +6,6 @@ from api.v1.views import app_views
 from models import storage
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
-    """ Route on object app_views that returns a JSON"""
-    return jsonify(status="OK")
-
-
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def get_stats():
     stats = {}
